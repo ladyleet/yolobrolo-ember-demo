531b440cb7ab6c23306ec8b23e5137d401f76cbc
Tracy Lee <tracy@wingman.events>
2016-08-09 07:05:57 -0700

    run

---
+++ app/components/video-card.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  model: [{
+      title: "Ember DND Helper",
+      people: "Taras Mankovski",
+      videoLink: "peNV2yJRMLo?rel=0"
+    },{
+      title: "Dependency Injection in Angular 2",
+      people: "Patrick J. Stapleton",
+      videoLink: "46WovCX8i-I?rel=0"
+    },{
+      title: "Angular CLI",
+      people: "Mike Brocchi",
+      videoLink: "BmZLpNRNnZo"
+    },{
+      title: "Angular Material 2 Spelunking & Issue Submission",
+      people: "Ben Lesh",
+      videoLink: "3gNsyL7wpXU"
+    },{
+      title: "Angular Material 2 Spelunking",
+      people: "Ben Lesh",
+      videoLink: "64PrKAQmXdk"
+    },{
+      title: "Template Driven Forms in Angular 2",
+      people: "Deborah Kurata",
+      videoLink: "iRz9WKfYy6U"
+    },{
+      title: "Contributing to RxJS",
+      people: "Ben Lesh",
+      videoLink: "jbNqD1OkIEY"
+    },{
+      title: "Contributing to Ember",
+      people: "Jorge Lainfiesta",
+      videoLink: "UoMg9Jm-V6k"
+    },{
+      title: "Functional Programming at a Bar",
+      people: "Ben Lesh",
+      videoLink: "Eh39B-DGKr8"
+    }]
+});
